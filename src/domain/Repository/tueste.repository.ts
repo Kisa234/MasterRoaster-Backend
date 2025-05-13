d637fdde645974f53bab20884658ad5627fa51f3
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2025-05-12 23:02:38 -0500

    Update tueste.repository.ts

--- src/domain/Repository/tueste.repository.ts
+++ src/domain/Repository/tueste.repository.ts
@@ -5,12 +5,11 @@ import { TuesteEntity } from "../entities/tueste.entity";
 
 export abstract class TuesteRepository {
     abstract createTueste(createTuesteDto:CreateTuesteDto): Promise<TuesteEntity>;
-        abstract getTuesteById(id: string): Promise<TuesteEntity | null>;
-        abstract updateTueste(id: string, updateTuesteDto:UpdateTuesteDto): Promise<TuesteEntity>;
-        abstract deleteTueste(id: string): Promise<TuesteEntity>;
-        abstract getTostadosByFecha(fecha: Date): Promise<TuesteEntity[]>;
-        abstract getAllTuestes(): Promise<TuesteEntity[]>;
-        abstract getTostadosByPedido(id_pedido: string): Promise<TuesteEntity[]>;
-        abstract completarTueste(id: string,completeTuesteDto:CompleteTuesteDto): Promise<TuesteEntity>;    
-        
-  }
+    abstract getTuesteById(id: string): Promise<TuesteEntity | null>;
+    abstract updateTueste(id: string, updateTuesteDto:UpdateTuesteDto): Promise<TuesteEntity>;
+    abstract deleteTueste(id: string): Promise<TuesteEntity>;
+    abstract getTostadosByFecha(fecha: Date): Promise<TuesteEntity[]>;
+    abstract getAllTuestes(): Promise<TuesteEntity[]>;
+    abstract getTostadosByPedido(id_pedido: string): Promise<TuesteEntity[]>;
+    abstract completarTueste(id: string,completeTuesteDto:CompleteTuesteDto): Promise<TuesteEntity>;            
+}
