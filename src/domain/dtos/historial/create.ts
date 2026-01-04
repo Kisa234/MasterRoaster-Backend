21aff0cfe6d30b6862d807b4e4916e8d85039029
Renzo Vilchez <110016263+Kisa234@users.noreply.github.com>
2026-01-04 15:11:53 -0500

    Update create.ts

--- src/domain/dtos/historial/create.ts
+++ src/domain/dtos/historial/create.ts
@@ -2,14 +2,16 @@ export class CreateHistorialDto {
     private constructor(
         public entidad: string,
         public id_entidad: string,
+        public id_user: string,
         public accion: string,
         public comentario: string,
         public objeto_antes?: any | null,
-        public id_user?: string,
     ) { }
 
     static create ( props: { [key: string]: any } ): [string?, CreateHistorialDto?] {
         const { entidad, id_entidad, id_user, accion, comentario, fecha_registro, objeto_antes } = props;
+
+        if (!id_user) return ['El id_user es requerido', undefined];    
         if (!entidad) return ['La entidad es requerida', undefined];
         if (!id_entidad) return ['El id_entidad es requerido', undefined];
         if (!accion) return ['La accion es requerida', undefined];
@@ -20,10 +22,10 @@ export class CreateHistorialDto {
             new CreateHistorialDto(
                 entidad,
                 id_entidad,
+                id_user,
                 accion,
                 comentario,
                 objeto_antes,
-                id_user,
             )
         ]
             
